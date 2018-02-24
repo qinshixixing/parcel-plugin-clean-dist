c4cc844c318935d4d05c8187ea1f86c6c4bbcc59
qinshixixing <qinshixixing@icloud.com>
2018-02-24 16:15:56 +0800

    Initialize the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# parcel-plugin-clean-dist
+This is a package a parcel plugin which delete the last bundled files before compile.
+
+## Installation
+```bash
+npm i -D # parcel-plugin-clean-dist
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+module.exports = () => {
+    let distDirName;
+    let outDirIndex = process.argv.indexOf('--out-dir');
+    if (outDirIndex > -1) {
+        distDirName = process.argv[outDirIndex + 1];
+    }
+    else {
+        distDirName = 'dist';
+    }
+    const absDistDirName = `${process.cwd()}/${distDirName}`;
+    try {
+        if (fs.statSync(absDistDirName).isDirectory()) {
+            const files = fs.readdirSync(absDistDirName);
+            files.forEach((file, index) => {
+                const filePath = `${absDistDirName}/${file}`;
+                fs.unlinkSync(filePath);
+            });
+        }
+    }
+    catch (err) {}
+};
